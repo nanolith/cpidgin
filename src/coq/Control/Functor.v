b335fc7a1b1ef439bfbf60d1b647c79d968f5b93
Justin Handville <nanolith@gmail.com>
2020-06-25 05:00:15 -0400

    Added Functor typeclass.

---
+++ src/coq/Control/Functor.v
@@ -0,0 +1,8 @@
+(* The Funtor typeclass provides a mapping function to lift an arbitrary      *)
+(* function to a functor space.                                               *)
+Class Functor (F: Type -> Type) : Type := {
+    fmap : forall {a b: Type}, (a -> b) -> F a -> F b
+}.
+
+(* The <$> operator maps to fmap. *)
+Infix "<$>" := fmap (at level 65, left associativity).
