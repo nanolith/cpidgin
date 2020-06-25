580d8a6ed81893e8b0a824e867ff58b3d87c6d9f
Justin Handville <nanolith@gmail.com>
2020-06-25 05:04:44 -0400

    Added Monad typeclass.

---
+++ src/coq/Control/Monad.v
@@ -0,0 +1,11 @@
+(* The Monad typeclass provides a way to capture a computation entering a     *)
+(* monadic space, as well as a way to lift values to this space and to bind   *)
+(* the outputs of these computations to functions that enter this monadic     *)
+(* space.                                                                     *)
+Class Monad (M: Type -> Type) : Type := {
+    mret : forall {t: Type}, t -> M t;
+    bind : forall {a b: Type}, M a -> (a -> M b) -> M b
+}.
+
+(* The >>= operator maps to bind. *)
+Infix ">>=" := bind (at level 65, left associativity).
