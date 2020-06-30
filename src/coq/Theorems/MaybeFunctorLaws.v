374380443d0e62dfdcabb9458f9d796c932f089e
Justin Handville <nanolith@gmail.com>
2020-06-30 04:22:27 -0400

    Updated proofs to match tutorial.

--- src/coq/Theorems/MaybeFunctorLaws.v
+++ src/coq/Theorems/MaybeFunctorLaws.v
@@ -2,25 +2,25 @@ Require Import CPidgin.Control.Functor.
 Require Import CPidgin.Data.Maybe.
 
 (* Helper function to act as an id function. *)
-Definition fid {A : Type} (x : A): A :=
+Definition id' {A : Type} (x : A): A :=
     x.
 
 (* Functor Law 1: Identity. *)
-Lemma Functor_Maybe_Law1:
+Lemma MaybeFunctorPreservesIdentityMorphisms:
     forall (A : Type) (x : Maybe A),
-        fid <$> x = x.
+        id' <$> x = x.
 Proof.
     intros.
     unfold fmap.
     unfold maybeFunctor.
-    unfold fid.
-    induction x.
+    unfold id'.
+    destruct x.
     trivial.
     trivial.
 Qed.
 
 (* Functor Law 2: Composition. *)
-Lemma Functor_Maybe_Law2:
+Lemma MaybeFunctorPreservesCompositionOfMorphisms:
     forall (A B C : Type) (x : Maybe A) (f : A -> B) (g : B -> C),
         (fun a => g (f a)) <$> x
             = g <$> (f <$> x).
@@ -28,7 +28,7 @@ Proof.
     intros.
     unfold fmap.
     unfold maybeFunctor.
-    induction x.
+    destruct x.
     trivial.
     trivial.
 Qed.
