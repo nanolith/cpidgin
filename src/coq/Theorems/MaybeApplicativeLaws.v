9ddb23ebbeb533fb1259904d02345c82adb5e526
Justin Handville <nanolith@gmail.com>
2020-06-30 04:20:25 -0400

    Updated proofs to match tutorial.

--- src/coq/Theorems/MaybeApplicativeLaws.v
+++ src/coq/Theorems/MaybeApplicativeLaws.v
@@ -2,26 +2,26 @@ Require Import CPidgin.Control.Applicative.
 Require Import CPidgin.Data.Maybe.
 
 (* Helper function to act as an id function. *)
-Definition fid {A : Type} (x : A) : A :=
+Definition id' {A : Type} (x : A) : A :=
     x.
 
 (* Applicative Law 1: Identity. *)
-Lemma Applicative_Maybe_Law1:
+Lemma MaybeApplicativePreservesIdentityMorphisms:
     forall (A : Type) (x : Maybe A),
-        pure fid <*> x = x.
+        pure id' <*> x = x.
 Proof.
     intros.
-    unfold fid.
+    unfold id'.
     unfold pure.
     unfold app.
     unfold maybeApplicative.
-    induction x.
+    destruct x.
     trivial.
     trivial.
 Qed.
 
 (* Applicative Law 2: Homomorphism. *)
-Lemma Applicative_Maybe_Law2:
+Lemma MaybeApplicativeHomomorphismLaw:
     forall (A B : Type) (x : A) (f : A -> B),
         pure f <*> pure x = pure (f x).
 Proof.
@@ -33,7 +33,7 @@ Proof.
 Qed.
 
 (* Applicative Law 3: Interchange. *)
-Lemma Applicative_Maybe_Law3:
+Lemma MaybeApplicativeInterchangeLaw:
     forall (A B : Type) (x : A) (f : Maybe (A -> B)),
         f <*> pure x
             = pure (fun g => g x) <*> f.
@@ -46,7 +46,7 @@ Proof.
 Qed.
 
 (* Applicative Law 4: Composition. *)
-Lemma Applicative_Maybe_Law4:
+Lemma MaybeApplicativeCompositionLaw:
     forall (A B C : Type) (w : Maybe A) (v : Maybe (A -> B))
            (u : Maybe (B -> C)),
         pure (fun f g x => f (g x)) <*> u <*> v <*> w
@@ -56,14 +56,10 @@ Proof.
     unfold pure.
     unfold app.
     unfold maybeApplicative.
-    induction w.
-    induction u.
-    induction v.
+    destruct u.
+    destruct v.
+    destruct w.
     trivial.
-    trivial.
-    trivial.
-    induction u.
-    induction v.
     trivial.
     trivial.
     trivial.
