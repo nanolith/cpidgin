a96fc7070e051ceea1064fe69ecb0d9b8a44f940
Justin Handville <nanolith@gmail.com>
2020-07-02 17:02:56 -0400

    Added proof of drop.

--- src/coq/Theorems/ListTheorems.v
+++ src/coq/Theorems/ListTheorems.v
@@ -40,3 +40,50 @@ Proof.
     unfold tail.
     trivial.
 Qed.
+
+(* helper to create a duplicate list of a given size. *)
+Fixpoint dupList {A : Type} (n : nat) (m : Maybe A) (ls : List A) : List A :=
+    match n with
+        | 0 => ls
+        | S n' => m :: dupList n' m ls
+    end.
+
+(* proof that we can unroll drop by 1. *)
+Lemma list_drop_unroll:
+    forall (A : Type) (n : nat) (x : Maybe A) (xs : List A),
+        drop (S n) (x :: xs) = drop n xs.
+Proof.
+    intros.
+    unfold drop.
+    trivial.
+Qed.
+
+(* proof that we can unroll dupList by 1. *)
+Lemma list_dupList_unroll:
+    forall (A : Type) (n : nat) (x : Maybe A) (xs : List A),
+        dupList (S n) x xs = x :: dupList n x xs.
+Proof.
+    intros.
+    unfold dupList.
+    trivial.
+Qed.
+
+(* proof that drop n drops n values from a list. *)
+Lemma list_drop:
+    forall (A : Type) (n : nat) (x : Maybe A) (xs : List A),
+        drop n (dupList n x xs) = xs.
+Proof.
+    intros.
+    induction n.
+    1: {
+        unfold drop.
+        unfold dupList.
+        trivial.
+    }
+    1: {
+        rewrite list_dupList_unroll.
+        rewrite list_drop_unroll.
+        rewrite IHn.
+        trivial.
+    }
+Qed.
