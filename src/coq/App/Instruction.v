fbdd437c8a51f4943c242af53f98f52e7c085e3f
Justin Handville <nanolith@gmail.com>
2020-11-02 02:27:35 -0500

    Added PUSH instruction.

--- src/coq/App/Instruction.v
+++ src/coq/App/Instruction.v
@@ -7,12 +7,16 @@ Import Bits.
 (* The Instruction inductive type represents a machine instruction. *)
 Inductive Instruction : Type :=
     | NOP : Instruction
-    | IMM : B64 -> Instruction.
+    | IMM : B64 -> Instruction
+    | PUSH : Instruction.
 
 (* The time to execute a NOP instruction. *)
 Definition NOP_DELAY : nat := 1.
 
 (* The time to execute an IMM instruction. *)
 Definition IMM_DELAY : nat := 2.
+
+(* The time to execute a PUSH instruction. *)
+Definition PUSH_DELAY : nat := 2.
 
 End Instruction.
