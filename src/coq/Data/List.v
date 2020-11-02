80968dcfbd70444552546303516e4a94c8c2ed24
Justin Handville <nanolith@gmail.com>
2020-11-01 21:40:33 -0500

    Added CPidgin.Data.List.

---
+++ src/coq/Data/List.v
@@ -0,0 +1,93 @@
+Require CPidgin.Data.Maybe.
+
+Module List.
+
+Import Maybe.Maybe.
+
+(* Simple linked list of values. *)
+Inductive List (A : Type) : Type :=
+    | nil : List A
+    | cons : A -> List A -> List A.
+
+(* Gather the implicit type A parameter from implicit context. *)
+Arguments nil {A}.
+(* The implicit type A maps to the type of the cons parameter. *)
+Arguments cons {A} a _.
+
+(* Use [] notation to indicate nil. *)
+Notation "[ ]" := nil (format "[ ]").
+(* Use [ x ] notation to indicate a list of one item. *)
+Notation "[ x ]" := (cons x nil).
+(* Use [x ; y ; .. ; z ] notation to indicate a variable length list. *)
+Notation "[ x ; y ; .. ; z ]" := (cons x (cons y .. (cons z nil) ..)).
+(* Use x :: y to break down a cons cell in a list. *)
+Notation "x :: y" := (cons x y).
+
+(* Get the head of a given List. *)
+Definition head {A : Type} (lst : List A) : Maybe A :=
+    match lst with
+        | [] => Nothing
+        | x :: xs => Just x
+    end.
+
+(* Get the tail of a given List. *)
+Definition tail {A : Type} (lst : List A) : List A :=
+    match lst with
+        | [] => []
+        | x :: xs => xs
+    end.
+
+(* Drop n elements from a List. *)
+Fixpoint drop {A : Type} (n : nat) (lst : List A) : List A :=
+    match n with
+        | 0 => lst
+        | S n' => drop n' (tail lst)
+    end.
+
+(* Append two Lists. *)
+Fixpoint append {A : Type} (l1 l2 : List A) : List A :=
+    match l1 with
+        | [] => l2
+        | (x :: xs) => x :: append xs l2
+    end.
+
+(* Use xs ++ ys to append these two lists. *)
+Notation "xs ++ ys" := (append xs ys).
+
+(* Get the length of a List. *)
+Fixpoint length {A : Type} (l : List A) : nat :=
+    match l with
+        | [] => 0
+        | (x :: xs) => S (length xs)
+    end.
+
+(* Take the first n elements in a List. *)
+Fixpoint take {A : Type} (n : nat) (l : List A) : List A :=
+    match n with
+        | 0 => []
+        | S n' =>
+            match l with
+                | [] => []
+                | (x :: xs) => x :: take n' xs
+            end
+    end.
+
+(* Take the nth item from a List. *)
+Definition nth {A : Type} (n : nat) (l : List A) : Maybe A :=
+    match n with
+        | 0 => head l
+        | S n' => head (drop n l)
+    end.
+
+(* Remove the nth item from a List. *)
+Fixpoint removeNth {A : Type} (n : nat) (l : List A) : List A :=
+    match n with
+        | 0 => tail l
+        | S n' =>
+        match l with
+            | [] => []
+            | (x :: xs) => x :: removeNth n' xs
+        end
+    end.
+
+End List.
