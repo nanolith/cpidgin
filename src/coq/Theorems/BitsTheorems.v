51c7a37de714ca9d98f8ba08fee9de5ace2a9d37
Justin Handville <nanolith@gmail.com>
2020-11-07 03:41:46 -0500

    Added B64_ge and proofs.

--- src/coq/Theorems/BitsTheorems.v
+++ src/coq/Theorems/BitsTheorems.v
@@ -283,6 +283,42 @@ Module ZStuff.
         trivial.
     Qed.
 
+    (* If two values are equal, then B64_ge returns 1. *)
+    Lemma B64_ge_Eq:
+        forall (x y : B64),
+            (B64_to_Z x) ?= (B64_to_Z y) = Eq ->
+                B64_ge x y = nat_to_B64 1.
+    Proof.
+        intros x y H.
+        unfold B64_ge.
+        rewrite H.
+        trivial.
+    Qed.
+
+    (* If two values are Lt, then B64_ge returns 0. *)
+    Lemma B64_ge_Lt:
+        forall (x y : B64),
+            (B64_to_Z x) ?= (B64_to_Z y) = Lt ->
+                B64_ge x y = nat_to_B64 0.
+    Proof.
+        intros x y H.
+        unfold B64_ge.
+        rewrite H.
+        trivial.
+    Qed.
+
+    (* If two values are Gt, then B64_ge returns 1. *)
+    Lemma B64_ge_Gt:
+        forall (x y : B64),
+            (B64_to_Z x) ?= (B64_to_Z y) = Gt ->
+                B64_ge x y = nat_to_B64 1.
+    Proof.
+        intros x y H.
+        unfold B64_ge.
+        rewrite H.
+        trivial.
+    Qed.
+
 End ZStuff.
 
 End BitsTheorems.
